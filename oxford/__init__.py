11cbc46a12abcfeea1796b2956feccb754b1c6ed
Ivan R. Judson <irjudson@gmail.com>
2016-02-14 10:26:29 -0800

    flake8

--- oxford/__init__.py
+++ oxford/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['emotion', 'face', 'person', 'persongroup', 'vision']
 
-from emotion import Emotion
-from face import Face
-from person import Person
-from persongroup import PersonGroup
-from vision import Vision
+from .emotion import Emotion
+from .face import Face
+from .person import Person
+from .persongroup import PersonGroup
+from .vision import Vision
