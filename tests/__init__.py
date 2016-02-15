b67365b087fd623732fef9e6a217af9ac077f17d
Ivan R. Judson <irjudson@gmail.com>
2016-02-14 21:32:14 -0800

    bah

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,9 @@
 __ALL__ = [ 'test_emotion', 'test_face', 'test_facelist', 
             'test_person', 'test_persongroup', 'test_vision' ]
+            
+from . import test_emotion
+from . import test_face
+from . import test_facelist
+from . import test_person
+from . import test_persongroup
+from . import test_vision
