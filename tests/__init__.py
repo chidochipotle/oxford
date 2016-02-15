899a25da99d3ae127dfd6b93202796526cbe76f5
Ivan R. Judson <irjudson@gmail.com>
2016-02-15 09:25:59 -0800

    changning unit test invocation to see if it works.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
 __ALL__ = [ 'test_emotion', 'test_face', 'test_facelist', 
             'test_person', 'test_persongroup', 'test_vision' ]
             
-from . import test_emotion
-from . import test_face
-from . import test_facelist
-from . import test_person
-from . import test_persongroup
-from . import test_vision
+import .test_emotion
+import .test_face
+import .test_facelist
+import .test_person
+import .test_persongroup
+import .test_vision
