52ed3adf93bc7f108dda8295c48f9f9a7f38b9ff
Ivan R. Judson <irjudson@gmail.com>
2016-02-15 09:28:37 -0800

    fidding with imports with coffee instead of flailing.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
 __ALL__ = [ 'test_emotion', 'test_face', 'test_facelist', 
             'test_person', 'test_persongroup', 'test_vision' ]
             
-import .test_emotion
-import .test_face
-import .test_facelist
-import .test_person
-import .test_persongroup
-import .test_vision
+import test_emotion
+import test_face
+import test_facelist
+import test_person
+import test_persongroup
+import test_vision
