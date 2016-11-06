4a7a44d786b66d8b6976c05457c4ffbe486fe3cf
Daniel Kornhauser <mr.kornhauser@gmail.com>
2016-11-06 16:42:01 -0500

    Fixing __init__ to import packages correctly in python 3

--- oxford/__init__.py
+++ oxford/__init__.py
@@ -1,1 +1,10 @@
-__ALL__ = ['emotion', 'face', 'facelist', 'person', 'persongroup', 'vision']
+print("loading package")
+from . import face
+from . import emotion
+from . import person
+from . import persongroup
+from . import facelist
+from . import vision
+from . import facemodel
+
+__all__ = ['emotion', 'face', 'facelist', 'person', 'persongroup', 'vision']
